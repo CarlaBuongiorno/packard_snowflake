4f609ce59d824c7e472e9d9b9fc8123a6e66e63c
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-10-14 12:37:48 +0200

    Add tests for zero and one generations

--- snowflake.py
+++ snowflake.py
@@ -1,2 +1,5 @@
-def snowflake():
-    pass
+def snowflake(number_of_generations):
+    if number_of_generations:
+        return [[number_of_generations]]
+    else:
+        return [[]]
--- test_snowflake.py
+++ test_snowflake.py
@@ -3,3 +3,11 @@ from snowflake import snowflake
 
 def test_snowflake_exists():
     assert snowflake
+
+
+def test_snowflake_0_generations_empty_grid():
+    assert snowflake(0) == [[]]
+
+
+def test_snowflake_1_generation_1_square():
+    assert snowflake(1) == [[1]]
