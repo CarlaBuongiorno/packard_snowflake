9c9dddee92a79d9812bc345711b24945065c1474
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-10-14 12:19:15 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from snowflake import snowflake
+
+
+def main():
+    packard_snowflake = snowflake()
+    print(packard_snowflake)
+
+if __name__ == '__main__':
+    main()
---
+++ test_snowflake.py
@@ -0,0 +1,5 @@
+from snowflake import snowflake
+
+
+def test_snowflake_exists():
+    assert snowflake
