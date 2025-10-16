95202cdd670d86c11a070e0bd487a85726d0ee10
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-10-16 10:12:19 +0200

    Add rows containing filled vertical center and complete rows with zeros

--- snowflake.py
+++ snowflake.py
@@ -4,9 +4,13 @@ import math
 def snowflake(number_of_generations):
     grid = []
     grid_size = (number_of_generations*2)-1
-    center_cross = math.ceil(grid_size/2)
+    half_number_of_generations = math.ceil(grid_size/2)
     if number_of_generations > 0:
         grid = add_center_row(grid, number_of_generations)
+        grid = build_rows_below_center(grid, half_number_of_generations)
+        grid = build_rows_above_center(grid, half_number_of_generations)
+        filled_rows = fill_rows_with_zeros(grid, half_number_of_generations)
+
         print(grid)
         return grid
     else:
@@ -16,4 +20,27 @@ def snowflake(number_of_generations):
 def add_center_row(grid, number_of_generations):
     center_row = [1 for i in range(number_of_generations*2-1)]
     grid.append(center_row)
+    return grid
+
+
+def build_rows_below_center(grid, half_number_of_generations):
+    for i in range(half_number_of_generations-1):
+        grid.append([1])
+    return grid
+
+
+def build_rows_above_center(grid, half_number_of_generations):
+    for i in range(half_number_of_generations-1):
+        grid.insert(0, [1])
+    return grid
+
+
+def fill_rows_with_zeros(grid, half_number_of_generations):
+    for i in range(half_number_of_generations-1):
+        for row in grid:
+            if row == grid[1]:
+                pass
+            else:
+                row.insert(0, 0)
+                row.append(0)
     return grid
