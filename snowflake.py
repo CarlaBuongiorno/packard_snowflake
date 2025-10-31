0be55a81de2705e51683783132416cf50015dad5
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-10-31 10:53:24 +0100

    Separate checking function to check_boundaries and to check for 1s touching 1s

--- snowflake.py
+++ snowflake.py
@@ -30,11 +30,28 @@ def build_grid(grid_size):
     return grid
     
 
+# def check_one_touches_one(grid, y, x):
+#     check_list = [
+#         len(grid)-1 > y and grid[y+1][x],
+#         len(grid)-1 > x and grid[y][x+1],
+#         y != 0 and grid[y-1][x],
+#         x != 0 and grid[y][x-1]
+#     ]
+#     return sum(check_list) == 1 or grid[y][x] == 1
+
+
 def check_one_touches_one(grid, y, x):
-    check_list = [
-        len(grid)-1 > y and grid[y+1][x],
-        len(grid)-1 > x and grid[y][x+1],
-        y != 0 and grid[y-1][x],
-        x != 0 and grid[y][x-1]
-    ]
-    return sum(check_list) == 1 or grid[y][x] == 1
+    '''Return True if a cell touches exactly one '1' (or is itself a '1').'''
+    return sum(
+        check_boundaries(grid, y, x, offset_y, offset_x)
+        and grid[y+offset_y][x+offset_x]
+        for offset_y, offset_x in [(-1, +0), (+1, +0), (+0, -1), (+0, +1)]
+    ) == 1 or grid[y][x]
+
+
+def check_boundaries(grid, y, x, y_boundary, x_boundary):
+    '''Return True if (y+y_boundary, x+x_boundary) is within the grid bounds.'''
+    return (
+        (y+y_boundary >= 0 and y+y_boundary < len(grid))
+        and (x+x_boundary >= 0 and x+x_boundary < len(grid))
+    )
